ef5860e5b6d157816c14837763d2abc1422b54d9
Chaga-Kariyawasam <chaganirmitha@gmail.com>
2024-02-06 23:28:10 +0530

    Corrections

--- react-admin/src/scenes/dashboard/index.jsx
+++ react-admin/src/scenes/dashboard/index.jsx
@@ -279,7 +279,7 @@ const Dashboard = () => {
             fontWeight="600"
             sx={{ marginBottom: "15px" }}
           >
-            Geography
+            Plants Geography
           </Typography>
           <Box height="200px">
             <GeographyChart isDashboard={true} />
