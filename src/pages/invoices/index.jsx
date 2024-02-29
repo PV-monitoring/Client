c8b76d23ad9af48f46cf17e773f6d0ad84a6989e
Nirmal-99 <nirmal99@gmail.com>
2024-02-29 17:58:42 +0530

    dynamic plant display

--- src/pages/invoices/index.jsx
+++ src/pages/invoices/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataInvoices } from "../../data/mockData";
+import { getMockDataInvoices, dynamicPlants } from "../../data/mockData";
 import Header from "../../components/Header";
 import Topbar from "../global/Topbar";
 import Sidebar from "../global/Sidebar";
@@ -13,17 +13,23 @@ const Invoices = () => {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
   const colors = tokens(theme.palette.mode);
+
+  // Use the function to get dynamic data
+  const dynamicData = dynamicPlants();
+  // Call the function to get transformed data based on dynamic data
+  const mockDataInvoices = getMockDataInvoices(dynamicData);
+
   const columns = [
-    { field: "id", headerName: "ID" },
+    { field: "id", headerName: "Plant ID" },
     {
       field: "name",
-      headerName: "Name",
+      headerName: "Plant Name",
       flex: 1,
       cellClassName: "name-column--cell",
     },
     {
       field: "location",
-      headerName: "Locationr",
+      headerName: "Plant Location",
       flex: 1,
     },
     {
@@ -32,18 +38,18 @@ const Invoices = () => {
       flex: 1,
     },
     {
-      field: "power",
-      headerName: "Power",
+      field: "status",
+      headerName: "Plant Status",
       flex: 1,
-      renderCell: (params) => (
-        <Typography color={colors.greenAccent[500]}>
-          {params.row.power}
-        </Typography>
-      ),
+      // renderCell: (params) => (
+      //   <Typography color={colors.greenAccent[500]}>
+      //     {params.row.power}
+      //   </Typography>
+      // ),
     },
     {
-      field: "date",
-      headerName: "Date",
+      field: "created_time",
+      headerName: "Created Time",
       flex: 1,
     },
   ];
@@ -56,41 +62,40 @@ const Invoices = () => {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
-    <Box m="20px">
-      <Header title="Plant Information" subtitle="List of Solar Plants" />
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-        }}
-      >
-        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
-      </Box>
-    </Box>
-    </main>
+            <Box m="20px">
+              <Header title="Plant Information" subtitle="List of Solar Plants" />
+              <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                  "& .MuiDataGrid-root": {
+                    border: "none",
+                  },
+                  "& .MuiDataGrid-cell": {
+                    borderBottom: "none",
+                  },
+                  "& .name-column--cell": {
+                    color: colors.greenAccent[300],
+                  },
+                  "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: colors.blueAccent[700],
+                    borderBottom: "none",
+                  },
+                  "& .MuiDataGrid-virtualScroller": {
+                    backgroundColor: colors.primary[400],
+                  },
+                  "& .MuiDataGrid-footerContainer": {
+                    borderTop: "none",
+                    backgroundColor: colors.blueAccent[700],
+                  },
+                  "& .MuiCheckbox-root": {
+                    color: `${colors.greenAccent[200]} !important`,
+                  },
+                }}>
+                <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+              </Box>
+            </Box>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
